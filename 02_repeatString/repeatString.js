f3957ce4ca8202cbb7b08d9013d87c80ce907d36
zhenhongswell <zhenhongswell@gmail.com>
2023-11-04 13:51:28 +0800

    01 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeatCount) {
+    if (repeatCount === (-1)){
+        return "ERROR";
+    }
+    let repeat_String="";
+    for (let index = 0; index < repeatCount; index++) {
+        repeat_String += string;
+    }
+    
+    return repeat_String;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
 const reverseString = function() {
 
 };
+// reverseString('hello')).toEqual('olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
