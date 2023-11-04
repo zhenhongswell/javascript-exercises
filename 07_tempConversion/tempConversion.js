03df09eb97ae944cbc9b8cfb9ef11219bcda5c75
zhenhongswell <zhenhongswell@gmail.com>
2023-11-04 15:54:27 +0800

    do leapyear and temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  // (x − 32) × 5/9 °C
+  value = (value - 32) * 5 / 9;
+  value = value.toFixed(1);
+  value = parseFloat(value);
+  return value;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  // (x × 9/5+ 32)°F
+  value = (value * 1.8) + 32;
+  value = value.toFixed(1);
+  value = parseFloat(value);
+  return value;
 };
 
 // Do not edit below this line
