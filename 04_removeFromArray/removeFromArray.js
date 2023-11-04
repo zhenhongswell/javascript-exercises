258e81f46bde04781e16b0834d607b9237940772
zhenhongswell <zhenhongswell@gmail.com>
2023-11-04 14:58:01 +0800

    Use indexOf and splice to remove match element.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(currentArray, ...elementsNeedToRemove) {
+    elementsNeedToRemove.forEach(element => {
+        let RemoveIndex = currentArray.indexOf(element);
+        if (RemoveIndex > -1){
+            currentArray.splice(RemoveIndex,1);
+        }
+    });
+    
+    return currentArray;
 };
-
+/*
+Loop currentArray
+    If 
+*/
 // Do not edit below this line
 module.exports = removeFromArray;
