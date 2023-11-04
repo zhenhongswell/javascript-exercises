063d2d1f182fe9571fdb1df8e18c5bce362a4f2f
zhenhongswell <zhenhongswell@gmail.com>
2023-11-04 15:20:32 +0800

    use type of and while to add sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(value1,value2) {
+    let sum = 0;
+    let max = 0;
+    let min = 0;
+    if (typeof(value1)!="number" || typeof(value2)!="number"){
+        return 'ERROR';
+    }
+    if (value1 >= value2){
+        max = value1;
+        min = value2;
+    }
+    else{
+        max = value2
+        min = value1;
+    }
+    sum = min;
+    while(min!=max){
+        sum+=max;
+        max--;
+    }
+    if (sum < 0 ) return 'ERROR';
+    return sum;
 };
 
 // Do not edit below this line
