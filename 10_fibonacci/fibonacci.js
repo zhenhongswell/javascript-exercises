c3e76d9729debb90506bba65b3332a3aeb1199d0
zhenhongswell <zhenhongswell@gmail.com>
2023-11-18 21:51:10 +0800

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    // if (typeof(value) === 'string'){
+    //     value = parseInt(value);
+    // }
+    // if (value < 0){
+    //     return "OOPS";
+    // }
+    // if (value === 0){
+    //     return 0;
+    // }
+    // if (value === 1){
+    //     return 1;
+    // }
+    // return fibonacci(value - 1) + fibonacci(value - 2);
+    const firstPrev = 1;
+    const secondPrev = 0;
+    for (let index = 2; index <= value; index++) {
+        const current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
 };
 
 // Do not edit below this line
