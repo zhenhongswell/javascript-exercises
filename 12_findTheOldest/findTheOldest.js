6e39d25037fdef54799b96244772dc676924b3ac
zhenhongswell <zhenhongswell@gmail.com>
2023-11-18 22:11:58 +0800

    12_findTheOldest using sort and get the first element after sorting array.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people = []) {
+    people.sort((lastPerson, nextPerson) => {
+        //if the lastPerson haven't died yet
+        if (lastPerson['yearOfDeath'] == null || lastPerson['yearOfDeath'] == undefined){
+            lastPerson['yearOfDeath'] = new Date().getFullYear();
+        }
+        //if the nextPerson haven't died yet
+        if (nextPerson['yearOfDeath'] == null || nextPerson['yearOfDeath'] == undefined){
+            nextPerson['yearOfDeath'] = new Date().getFullYear();
+        }
+        // calculate the lived time,and the person is already died
+        const lastPersonLived = lastPerson['yearOfDeath'] - lastPerson['yearOfBirth'];
+        const nextPersonLived = nextPerson['yearOfDeath'] - nextPerson['yearOfBirth'];
 
+        //check who lives longer.
+        if (lastPersonLived < nextPersonLived) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    })
+    return people[0];
 };
 
 // Do not edit below this line
