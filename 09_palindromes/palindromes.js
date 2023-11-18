92664a36065fc7343008958bfc026eca93d6fc5a
zhenhongswell <zhenhongswell@gmail.com>
2023-11-18 21:32:11 +0800

    solved 09_palindromes use methods: tolowercase repalace the punctuation with empy string finally use while loop to check the string is palindromes or not.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string="") {
+    string = string
+                .replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g,'')
+                .replaceAll(' ','')
+                .toLowerCase();
+    let index_1 = 0;
+    let index_2 = string.length - 1;
+    while(index_1 <= index_2){
+        if (string[index_1])
+        if (string[index_1] != string[index_2]) return false;
+        index_1++;
+        index_2--;
+    }
+    return true;
 };
 
 // Do not edit below this line
