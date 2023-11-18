5de6d9bad87f3d56dc70f8726bb761171367ea4b
zhenhongswell <zhenhongswell@gmail.com>
2023-11-18 21:54:09 +0800

    fix const to let

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,6 @@
 const fibonacci = function(value) {
+    if (value < 0) return "OOPS";
+    if (value === 0) return 0;
     // if (typeof(value) === 'string'){
     //     value = parseInt(value);
     // }
@@ -12,13 +14,14 @@ const fibonacci = function(value) {
     //     return 1;
     // }
     // return fibonacci(value - 1) + fibonacci(value - 2);
-    const firstPrev = 1;
-    const secondPrev = 0;
+    let firstPrev = 1;
+    let secondPrev = 0;
     for (let index = 2; index <= value; index++) {
-        const current = firstPrev + secondPrev;
+        let current = firstPrev + secondPrev;
         secondPrev = firstPrev;
         firstPrev = current;
     }
+    return firstPrev;
 };
 
 // Do not edit below this line
