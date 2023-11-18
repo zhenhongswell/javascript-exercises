6f73ccd20d60b10d7f3f730882057f7e236aac61
zhenhongswell <zhenhongswell@gmail.com>
2023-11-18 22:01:06 +0800

    11_getTheBookTitles using map method to creat a new array for just the book 'title'.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books=[]) {
+    const newbooks = books.map(book => book['title']);
+    return newbooks;
 };
 
 // Do not edit below this line
